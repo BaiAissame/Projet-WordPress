a0983a886a910c198bce7e8cda5320e873fac4e3
Danny Fu <dfu@myges.fr>
2025-07-23 20:59:36 +0200

    service

---
+++ themes/ESGI-theme/template-parts/services/service.php
@@ -0,0 +1,7 @@
+<div class="service-container">
+    <h2 class="service-title">Corp. Parties</h2>
+    <p class="about-description">
+        Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution. </p>
+
+</div>
+<img class="service-page-footer" src="<?php echo get_template_directory_uri(); ?>/images/service-page-footer.png" loading="lazy">            
