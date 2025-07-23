e5e7a9dcfcb43891e6d0e63a7b96341c84dfda15
Danny Fu <dfu@myges.fr>
2025-07-23 20:59:03 +0200

    about

---
+++ themes/ESGI-theme/template-parts/about/hero-about.php
@@ -0,0 +1,11 @@
+<section class="events-section">    
+    <h1 class="about-title">About us.</h1>
+    <div class="hero-home-container"> 
+                <div class="hero-about-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/hero-about-icon.png" loading="lazy">            
+                    <div class="about-content">
+                    <h2 class="about-title">Sky's the limit</h2>
+                    <p class="about-description">
+                    Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution.                 </div>
+            </div>
+</section>
---
+++ themes/ESGI-theme/template-parts/about/hero-team.php
@@ -0,0 +1,25 @@
+<section class="events-section">
+    <h1 class="about-title">Our Team</h1>
+    <div class="hero-team-container">
+        <div class="teammate">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/teammate1.png" loading="lazy">
+            <h1>Sales Manager</h1>
+            <p>+33 1 53 31 25 23<br>sales@company.com</p>
+        </div>
+        <div class="teammate">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/teammate2.png" loading="lazy">
+            <h1>Event planner</h1>
+            <p>+33 1 53 31 25 24<br>plan@company.com</p>
+        </div>
+        <div class="teammate">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/teammate3.png" loading="lazy">
+            <h1>Designer</h1>
+            <p>+33 1 53 31 25 20<br>design@company.com</p>
+        </div>
+        <div class="teammate">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/teammate4.png" loading="lazy">
+            <h1>CEO</h1>
+            <p>+33 1 53 31 25 25<br>ceo@company.com</p>
+        </div>
+    </div>
+</section>
