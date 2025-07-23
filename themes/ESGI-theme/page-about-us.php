a8e7500d85fafdf01eb48eda86e121aa4b154517
BAI Aissame <Bai_a@outlook.fr>
2025-07-23 20:28:03 +0200

    page about

---
+++ themes/ESGI-theme/page-about-us.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<?php include('template-parts/about/hero-about.php'); ?>
+
+<?php include('template-parts/about/hero-identity-about.php'); ?>
+
+<?php include('template-parts/about/hero-team.php'); ?>
+
+
+<?php get_footer(); ?>
