4d0004fb2c1f0067f2beb6254321a703ee3cc315
Bai Aissame <Bai_a@outlook.fr>
2025-07-24 11:11:48 +0200

    Correction partners + nav bloque input

--- themes/ESGI-theme/template-parts/home/partners-home.php
+++ themes/ESGI-theme/template-parts/home/partners-home.php
@@ -1,15 +1,15 @@
-<!-- Section Partners -->
-<section>
-    <div>
-        <h2 class="partner-title">Our Partners</h2>
-        <div class="partner-list-img">
-            <?php for ($i = 1; $i <= 6; $i++): ?>
-                <?php $logo = get_theme_mod("esgi_partner_logo_$i"); ?>
-                <?php if ($logo): ?>
-                    <img src="<?php echo esc_url($logo); ?>" loading="lazy" alt="Logo partenaire <?php echo $i; ?>"
-                        class="partner-logo">
-                <?php endif; ?>
-            <?php endfor; ?>
+    <!-- Section Partners -->
+    <section >
+        <div >
+            <h2 class="partner-title" >Our Partners</h2>
+            <div class="partner-list-img" >
+                <!-- Partner logos - remplacez par vos vraies images -->
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-1.svg" loading="lazy">            
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-2.svg" loading="lazy">            
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-3.svg" loading="lazy">            
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-4.svg" loading="lazy">            
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-5.svg" loading="lazy">            
+                <img src="<?php echo get_template_directory_uri(); ?>/images/partner-6.svg" loading="lazy">            
+            </div>
         </div>
-    </div>
-</section>
+    </section>
